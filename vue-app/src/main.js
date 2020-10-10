669d7fc7d51e02ee57968262aab7e7559938fb50
Igor Podgainõi <podgainoi.igor@yandex.com>
2020-10-10 15:29:35 +0300

    Change coding style

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -1,11 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    render: function(createElem) {
+        return createElem(App);
+    }
+}).$mount("#app");
