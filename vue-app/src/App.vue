f0b9e85f4177021ffb3a2c8edd3774058049b711
Igor Podgainõi <podgainoi.igor@yandex.com>
2020-10-13 16:07:19 +0300

    Implement brief statistics

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -11,6 +11,9 @@
                 <card-invite header="Invite new members"
                              subtitle="Have anyone missing? Just add them using the form below."
                              :team-names="['SuperTeam', 'Company Inc.', 'Europe Sales', 'Asia Sales']"/>
+                <card header="Header" subtitle="Subtitle">
+                    <statistics-brief :data="statisticsData()"/>
+                </card>
             </div>
         </div>
         <bottom-bar>
@@ -26,6 +29,7 @@ import BottomBar from "@/components/BottomBar.vue";
 import Breadcrumbs from "@/components/Breadcrumbs.vue";
 import Card from "@/components/card/Card.vue";
 import CardInvite from "@/components/card/CardInvite.vue";
+import StatisticsBrief from "@/components/StatisticsBrief.vue";
 
 export default {
     components: {
@@ -34,7 +38,8 @@ export default {
         BottomBar,
         Breadcrumbs,
         Card,
-        CardInvite
+        CardInvite,
+        StatisticsBrief
     },
     methods: {
         hierarchy: function() {
@@ -50,6 +55,22 @@ export default {
                 {
                     title: "Testing3",
                     url: "link3"
+                }
+            ];
+        },
+        statisticsData: function() {
+            return [
+                {
+                    "left": ["5.3 hrs", "Avg Members Sessions", "+18.68%", "vs in last 7 days"],
+                    "right": ["6.4 hrs", "Avg Members Sessions", "+23.54%", "vs in last 7 days"]
+                },
+                {
+                    "left": ["2.1 hrs", "Avg Members Sessions", "+56.21%", "vs in last 7 days"],
+                    "right": ["4.9 hrs", "Avg Members Sessions", "+45.34%", "vs in last 7 days"]
+                },
+                {
+                    "left": ["11.2 hrs", "Avg Members Sessions", "+56.43%", "vs in last 7 days"],
+                    "right": ["0.4 hrs", "Avg Members Sessions", "+98.32%", "vs in last 7 days"]
                 }
             ];
         }
---
+++ vue-app/src/components/StatisticsBrief.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="the-statistics">
+        <div v-for="(stat, i) in data" :key="stat"
+             :class="(i === 0) ? 'row mb-5 mt-2' : ((i+1 !== data.length) ? 'row mb-5' : 'row mb-3')">
+            <div class="col-1 align-self-center text-black">
+                <i class="icon-list ml-3"></i>
+            </div>
+            <div class="col-md-6 col-lg-5 align-self-center text-black">
+                <h4 class="mb-0 font-26">{{ stat.left[0] }}</h4>
+                <p class="mb-2">{{ stat.left[1] }}</p>
+                <p class="mb-0">
+                    <span class="f-w-7">{{ stat.left[2] }}</span>&nbsp;
+                    <span class="font-12">{{ stat.left[3] }}</span>
+                </p>
+            </div>
+            <div class="col-md-6 col-lg-5 align-self-center text-black">
+                <h4 class="mb-0 font-26">{{ stat.right[0] }}</h4>
+                <p class="mb-2">{{ stat.right[1] }}</p>
+                <p class="mb-0">
+                    <span class="f-w-7">{{ stat.right[2] }}</span>&nbsp;
+                    <span class="font-12">{{ stat.right[3] }}</span>
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "StatisticsBrief",
+    props: {
+        data: Array
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.h4, h4 {
+    font-weight: 700;
+}
+
+h4 {
+    font-size: 24px;
+}
+
+.font-12 {
+    font-size: 12px;
+}
+
+.font-26 {
+    font-size: 26px;
+}
+
+.f-w-7 {
+    font-weight: 700;
+}
+
+.text-black {
+    color: #2B343A !important;
+}
+
+@media (max-width: 991px) {
+    h4 {
+        font-size: 22px;
+    }
+}
+
+@media (max-width: 767px) {
+    h4 {
+        font-size: 20px;
+    }
+}
+
+</style>
