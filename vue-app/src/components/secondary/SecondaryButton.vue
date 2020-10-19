fcd2a7859b3061a4f154da8f01ff7bbd2055b680
Igor Podgainõi <podgainoi.igor@yandex.com>
2020-10-19 12:52:26 +0300

    Refactored SecondaryButton.vue SCSS

--- vue-app/src/components/secondary/SecondaryButton.vue
+++ vue-app/src/components/secondary/SecondaryButton.vue
@@ -14,5 +14,39 @@ export default {
 </script>
 
 <style scoped lang="scss">
+$color-button-primary: #346bf1;
+$color-button-success: #27b866;
+
+%button-primary {
+    background-color: $color-button-primary;
+    border-color: $color-button-primary;
+}
+
+%button-success {
+    background-color: $color-button-success;
+    border-color: $color-button-success;
+}
+
+.btn {
+    &-primary {
+        &:active { @extend %button-primary; }
+        &:hover { @extend %button-primary; }
+        &:focus { @extend %button-primary; }
+        &:visited { @extend %button-primary; }
+        &:not(:disabled):not(.disabled):active {
+            @extend %button-primary;
+        }
+    }
+
+    &-success {
+        &:active { @extend %button-success; }
+        &:hover { @extend %button-success; }
+        &:focus { @extend %button-success; }
+        &:visited { @extend %button-success; }
+        &:not(:disabled):not(.disabled):active {
+            @extend %button-success;
+        }
+    }
+}
 
 </style>
