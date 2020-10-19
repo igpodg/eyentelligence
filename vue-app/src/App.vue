43ba833d682b1dfcf5b3ca4dfec5d25b2047ae2a
Igor Podgainõi <podgainoi.igor@yandex.com>
2020-10-19 19:30:07 +0300

    Make fonts static (don't require Internet)

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -87,7 +87,8 @@ export default {
 </script>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css?family=Comfortaa:300,400,700");
+//@import url("https://fonts.googleapis.com/css?family=Comfortaa:300,400,700");
+@import "./fonts/comfortaa";
 
 $s-size: 600px;
 $m-size: 768px;
