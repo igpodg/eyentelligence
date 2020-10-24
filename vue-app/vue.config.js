2befafed5931da0d64fdd2dc9b52f4f3f5cc637b
Igor Podgainõi <podgainoi.igor@yandex.com>
2020-10-24 12:32:37 +0300

    Moved "img" to the "assets" directory (manifest)

--- vue-app/vue.config.js
+++ vue-app/vue.config.js
@@ -6,6 +6,32 @@ module.exports = {
             appleTouchIcon: "assets/img/icons/apple-touch-icon-152x152.png",
             maskIcon: "assets/img/icons/safari-pinned-tab.svg",
             msTileImage: "assets/img/icons/msapplication-icon-144x144.png"
+        },
+        manifestOptions: {
+            icons: [
+                {
+                    src: "./assets/img/icons/android-chrome-192x192.png",
+                    sizes: "192x192",
+                    type: "image/png"
+                },
+                {
+                    src: "./assets/img/icons/android-chrome-512x512.png",
+                    sizes: "512x512",
+                    type: "image/png"
+                },
+                {
+                    src: "./assets/img/icons/android-chrome-maskable-192x192.png",
+                    sizes: "192x192",
+                    type: "image/png",
+                    purpose: "maskable"
+                },
+                {
+                    src: "./assets/img/icons/android-chrome-maskable-512x512.png",
+                    sizes: "512x512",
+                    type: "image/png",
+                    purpose: "maskable"
+                }
+            ]
         }
     }
 };
