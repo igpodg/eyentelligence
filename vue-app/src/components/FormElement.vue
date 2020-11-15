2ef0b677971ccaeee4d3282235c9d59af5133f76
Igor Podgainõi <podgainoi.igor@yandex.com>
2020-11-15 15:40:02 +0200

    [EYEN-76]: implemented a list of Dashboards inside TeamHome

---
+++ vue-app/src/components/FormElement.vue
@@ -0,0 +1,66 @@
+<template>
+    <div :class="'card inner-card' + (last ? ' last' : '')">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-3"><i class="icon-list"></i></div>
+                <div class="col-9 label">{{ label }}</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FormElement",
+    props: {
+        label: String,
+        last: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+$color-main-text: #2b343a;
+$color-vertical-line: #adadad;
+$color-shadow: #c8c8c8;
+
+.inner-card {
+    width: 100% !important;
+    margin-bottom: 20px;
+    box-shadow: 0 0 30px 0 rgba($color-shadow, 0.22);
+    cursor: pointer;
+    &.last {
+        margin-bottom: 10px;
+    }
+
+    .card-body {
+        padding: 0.75rem 1.25rem !important;
+
+        .row {
+            >div:first-child {
+                border-right: 1.5px solid $color-vertical-line;
+                text-align: center;
+
+                i {
+                    position: relative;
+                    top: 30%;
+                }
+            }
+
+            .label {
+                padding-top: 0.8rem !important;
+                padding-bottom: 0.8rem !important;
+                //text-align: left;
+
+                font-size: 16px;
+                font-weight: 700;
+                color: $color-main-text !important;
+            }
+        }
+    }
+}
+
+</style>
--- vue-app/src/views/TeamHome.vue
+++ vue-app/src/views/TeamHome.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="team-home">
         <div class="row">
+            <div class="col-md-12 col-lg-12 col-xl-5">
+                <card heading="Dashboards" subtitle="See a list of all dashboards available for the team.">
+                    <form-element label="My Dashboard"/>
+                    <form-element label="My Dashboard #2" last/>
+                </card>
+                <card heading="Statistics (general)" subtitle="The brief statistics of usage.
+                    For more information, visit the appropriate section.">
+                </card>
+            </div>
             <div class="col-md-12 col-lg-12 col-xl-7">
                 <card heading="Last Activity" subtitle="Here you can see what recent actions
                     have been taken by the team members.">
@@ -9,12 +18,15 @@
                                       tip="Resized tile &quot;Yearly&quot;" time="Today, 14:32"/>
                         <hr>
                         <activity-row name="John Johnson" :rank="2" small
-                                      tip="Changed &quot;My Dashboard #2&quot; visibility" time="Mon, 08:50"/>
+                                      tip="Changed &quot;My Dashboard #2&quot; visibility" time="Mon, 09:46"/>
                         <activity-row name="Emily Watson" :rank="3" small
-                                      tip="No activity" time="Mon, 09:46"/>
+                                      tip="No activity" time="Mon, 08:50"/>
                         <activity-row name="Mike Anders" :rank="4" small last
                                       tip="Resized tile &quot;Monthly Sales&quot;" time="Thu, 16:26"/>
                     </div>
+                </card>
+                <card heading="Statistics (general)" subtitle="The brief statistics of usage.
+                    For more information, visit the appropriate section.">
                 </card>
             </div>
         </div>
@@ -24,12 +36,14 @@
 <script>
 import Card from "@/components/card/Card.vue";
 import ActivityRow from "@/components/ActivityRow.vue";
+import FormElement from "@/components/FormElement.vue";
 
 export default {
     name: "TeamHome",
     components: {
         Card,
-        ActivityRow
+        ActivityRow,
+        FormElement
     }
 }
 </script>
