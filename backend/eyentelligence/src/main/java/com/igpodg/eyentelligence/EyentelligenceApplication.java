c9b6a65850bc37f4c175bf9740e1c27b74572cbe
Igor Podgainõi <podgainoi.igor@yandex.com>
2020-11-17 01:08:12 +0200

    [EYEN-63]: initial commit for Spring backend

---
+++ backend/eyentelligence/src/main/java/com/igpodg/eyentelligence/EyentelligenceApplication.java
@@ -0,0 +1,11 @@
+package com.igpodg.eyentelligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyentelligenceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EyentelligenceApplication.class, args);
+	}
+}
---
+++ backend/eyentelligence/src/main/java/com/igpodg/eyentelligence/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.igpodg.eyentelligence.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String testController() {
+        return "Test OK";
+    }
+}
