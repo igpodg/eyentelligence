4454feb393ddbbd53dd60d3ecee42cf42c27f6c4
Igor Podgainõi <podgainoi.igor@yandex.com>
2020-11-16 20:48:22 +0200

    [EYEN-6]: implement registration

--- vue-app/src/views/InvitedRegister.vue
+++ vue-app/src/views/InvitedRegister.vue
@@ -1,18 +1,21 @@
 <template>
-    <secondary-form title="You've been invited to Eyentelligence!"
-                    subtitle="Invited by: SuperTeam (John Johnson)">
+    <secondary-form :title="isRegister ? registerTitle : invitedTitle"
+                    :subtitle="isRegister ? undefined : 'Invited by: SuperTeam (John Johnson)'">
         <secondary-link url="/login" tall>I already have an account</secondary-link>
         <secondary-main-text>
             Please enter your desired credentials to register
             in the system and join your colleagues:
         </secondary-main-text>
-        <secondary-input id="username" name="Username" required/>
-        <secondary-input id="password" name="Password" type="password" required/>
+        <secondary-dropdown id="team" name="Team to join"
+                            :data="availableTeams" v-if="isRegister"/>
+        <hr v-if="isRegister">
+        <secondary-input id="username" name="Username" is-required/>
+        <secondary-input id="password" name="Password" type="password" is-required/>
         <div class="form-row">
             <secondary-dropdown :cols="3" id="title" name="Title"
                                 :data="['Mr.', 'Mrs.', 'Ms.', 'Dr.']"/>
-            <secondary-input :cols="4" id="firstname" name="First name" required/>
-            <secondary-input :cols="5" id="lastname" name="Last name" required/>
+            <secondary-input :cols="4" id="firstname" name="First name" is-required/>
+            <secondary-input :cols="5" id="lastname" name="Last name" is-required/>
         </div>
         <secondary-button color="success" name="Done"/>
     </secondary-form>
@@ -35,6 +38,19 @@ export default {
         SecondaryDropdown,
         SecondaryInput,
         SecondaryButton
+    },
+    data: function() {
+        return {
+            availableTeams: [],
+            isRegister: false,
+            invitedTitle: "You've been invited to Eyentelligence!",
+            registerTitle: "Register in Eyentelligence"
+        }
+    },
+    created: function() {
+        this.availableTeams = this.$root.$children[0]
+            .teams.map(team => team.name);
+        this.isRegister = this.$route.name === "Register";
     }
 }
 </script>
